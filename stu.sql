e97037ca63ac1966d897ee09fb368e3be99e76e5
wz <2530725964@qq.com>
2019-09-17 10:44:23 +0800

    add dict

---
+++ stu.sql
@@ -0,0 +1,155 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: stu
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `stu`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `stu` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `stu`;
+
+--
+-- Table structure for table `class_1`
+--
+
+DROP TABLE IF EXISTS `class_1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `class_1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `age` tinyint(3) unsigned NOT NULL,
+  `gender` enum('m','w','o') DEFAULT NULL,
+  `tel` char(16) DEFAULT NULL,
+  `score` float DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `class_1`
+--
+
+LOCK TABLES `class_1` WRITE;
+/*!40000 ALTER TABLE `class_1` DISABLE KEYS */;
+INSERT INTO `class_1` VALUES (1,'Alex',17,'m',NULL,88.5),(2,'Abby',18,'w',NULL,91),(3,'Tom',19,'m',NULL,79),(4,'Lucy',19,'w',NULL,60);
+/*!40000 ALTER TABLE `class_1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `interest`
+--
+
+DROP TABLE IF EXISTS `interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `hobby` set('sing','dance','draw') DEFAULT NULL,
+  `level` char(1) DEFAULT NULL,
+  `price` decimal(7,2) DEFAULT NULL,
+  `comment` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `interest`
+--
+
+LOCK TABLES `interest` WRITE;
+/*!40000 ALTER TABLE `interest` DISABLE KEYS */;
+INSERT INTO `interest` VALUES (1,'Joy','sing,dance','B',13800.00,'骨骼惊奇，舞林高手');
+/*!40000 ALTER TABLE `interest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marathon`
+--
+
+DROP TABLE IF EXISTS `marathon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marathon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `athlete` varchar(32) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `registration_time` datetime DEFAULT NULL,
+  `performance` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marathon`
+--
+
+LOCK TABLES `marathon` WRITE;
+/*!40000 ALTER TABLE `marathon` DISABLE KEYS */;
+INSERT INTO `marathon` VALUES (1,'曹操','1992-05-06','2019-06-08 14:23:50','02:36:48'),(2,'刘备','1993-05-06','2020-06-06 14:23:50','13:36:48'),(3,'关羽','1998-07-01','2000-06-06 04:23:50','01:36:48');
+/*!40000 ALTER TABLE `marathon` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `books`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `books` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `books`;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) NOT NULL,
+  `author` varchar(32) DEFAULT '匿名',
+  `price` decimal(5,2) DEFAULT NULL,
+  `publication_date` date DEFAULT NULL,
+  `publication` varchar(256) DEFAULT NULL,
+  `comment` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'老人于海','海明威',38.50,'2002-01-12','机械工业出版社','带给你勇气'),(3,'呐喊','鲁迅',36.50,'2017-06-06','中国人民出版社','你是否已经麻木'),(4,'骆驼祥子','老舍',88.00,'2017-06-06','中国人民出版社','你是“祥子”吗？');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-16 11:56:16
